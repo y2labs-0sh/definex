243a677d8b89b4d929b9669be4ce29346db7c716
kaigedong <dongkaige@gmail.com>
2020-04-03 21:58:57 +0800

    add license to deposit-loan

--- pallets/deposit-loan/src/lib.rs
+++ pallets/deposit-loan/src/lib.rs
@@ -1,3 +1,20 @@
+// Copyright (C) 2020 by definex.io
+
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+// This module is meant for Web3 grant. In this module, definex implemented a DeFi model which follows a 'maker-taker'.
+
 // use new_oracle to get btc price
 // Notice：the btc price used here is consered as two assets exchange ratio.
 // let current_price = <new_oracle::Module<T>>::current_price(&token);
