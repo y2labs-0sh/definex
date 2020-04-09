fdb315c038c77b5781eaeb6a5da9f4c1ceb4d7ad
kaigedong <dongkaige@gmail.com>
2020-04-09 16:59:01 +0800

    feat:add interest testcase

--- pallets/deposit-loan/src/tests.rs
+++ pallets/deposit-loan/src/tests.rs
@@ -41,7 +41,9 @@ fn unittest_works() {
 
 fn next_n_block(n: <Test as system::Trait>::BlockNumber) {
     SystemTest::set_block_number(SystemTest::block_number() + n);
-    DepositLoanTest::on_finalize(SystemTest::block_number());
+    // std::time::Duration::from_secs(3 * n);
+    DepositLoanTest::on_initialize(SystemTest::block_number());
+    // DepositLoanTest::on_finalize(SystemTest::block_number());
 }
 
 #[test]
@@ -74,8 +76,6 @@ fn staking_redeem_works() {
         // check status
         assert_eq!(GenericAssetTest::free_balance(&USDT, &dave), 4000);
         assert_eq!(GenericAssetTest::free_balance(&USDT, &DepositLoanTest::collection_account_id()), 1000);
-        assert_eq!(DepositLoanTest::market_dtoken(), 1000);
-        assert_eq!(DepositLoanTest::total_dtoken(), 1000);
         assert_eq!(DepositLoanTest::user_dtoken(dave.clone()), 1000);
 
         // dave deposit 500 unit usdt
@@ -84,8 +84,6 @@ fn staking_redeem_works() {
         // check status
         assert_eq!(GenericAssetTest::free_balance(&USDT, &dave), 4500);
         assert_eq!(GenericAssetTest::free_balance(&USDT, &DepositLoanTest::collection_account_id()), 500);
-        assert_eq!(DepositLoanTest::market_dtoken(), 500);
-        assert_eq!(DepositLoanTest::total_dtoken(), 500);
         assert_eq!(DepositLoanTest::user_dtoken(dave.clone()), 500);
 
         // mint 5000 unit usdt to eve
@@ -109,8 +107,6 @@ fn staking_redeem_works() {
         // check status
         assert_eq!(GenericAssetTest::free_balance(&USDT, &eve), 4600);
         assert_eq!(GenericAssetTest::free_balance(&USDT, &DepositLoanTest::collection_account_id()), 900);
-        assert_eq!(DepositLoanTest::market_dtoken(), 900);
-        assert_eq!(DepositLoanTest::total_dtoken(), 900);
         assert_eq!(DepositLoanTest::user_dtoken(eve.clone()), 400);
 
         assert_ok!(DepositLoanTest::make_redeem(&eve, &USDT, &DepositLoanTest::collection_account_id(), 400));
@@ -166,11 +162,11 @@ fn apply_draw_addcollateral_repay_works() {
         // eve have 300 unit usdt；180 unit btc
         assert_eq!(GenericAssetTest::free_balance(&BTC, &eve), 180);
         assert_eq!(GenericAssetTest::free_balance(&USDT, &eve), 300);
-        // pawn_sho have 20 unit btc
+        // pawn_shop have 20 unit btc
         assert_eq!(GenericAssetTest::free_balance(&BTC, &DepositLoanTest::pawn_shop()), 20);
 
         assert_eq!(DepositLoanTest::total_loan(), 300);
-        
+
         // current Utilization rate 300.0/24300 = 0.012345679012345678
         // current borrow interest rate：0.05123456790123457
         assert_eq!(DepositLoanTest::current_loan_interest_rate(), 5123456);
@@ -202,5 +198,84 @@ fn apply_draw_addcollateral_repay_works() {
             10,
         ));
 
+    });
+}
+
+
+#[test]
+fn deliver_interest_works() {
+    let root: <Test as system::Trait>::AccountId = get_from_seed::<sr25519::Public>("Root");
+    let dave: <Test as system::Trait>::AccountId = get_from_seed::<sr25519::Public>("Dave");
+    let eve: <Test as system::Trait>::AccountId = get_from_seed::<sr25519::Public>("Eve");
+    let frank: <Test as system::Trait>::AccountId = get_from_seed::<sr25519::Public>("Frank");
+
+    ExtBuilder::default().build().execute_with(|| {
+
+        // mint for dave 200_0000_0000 unit usdt:
+        assert_ok!(GenericAssetTest::mint_free(
+            &USDT,
+            &root,
+            &dave,
+            &<<Test as generic_asset::Trait>::Balance as TryFrom<u64>>::try_from(200_0000_0000)
+                .ok()
+                .unwrap(),
+        ));
+
+        // mint for eve 200_0000_0000 unit usdt:
+        assert_ok!(GenericAssetTest::mint_free(
+            &USDT,
+            &root,
+            &eve,
+            &<<Test as generic_asset::Trait>::Balance as TryFrom<u64>>::try_from(200_0000_0000)
+                .ok()
+                .unwrap(),
+        ));
+
+        // mint for frank 200_0000_0000 unit btc:
+        assert_ok!(GenericAssetTest::mint_free(
+            &BTC,
+            &root,
+            &frank,
+            &<<Test as generic_asset::Trait>::Balance as TryFrom<u64>>::try_from(200_0000_0000)
+                .ok()
+                .unwrap(),
+        ));
+
+        // dave deposit 100_0000_0000 unit USDT
+        assert_ok!(DepositLoanTest::create_staking(
+            dave.clone(),
+            USDT.clone(),
+            <<Test as generic_asset::Trait>::Balance as TryFrom<u64>>::try_from(100_0000_0000)
+                .ok()
+                .unwrap()
+        ));
+
+        assert_ok!(DepositLoanTest::apply_for_loan(
+            frank.clone(),
+            20_0000_0000,
+            40_0000_0000,
+        ));
+
+        assert_eq!(DepositLoanTest::total_loan(), 40_0000_0000);
+        assert_eq!(DepositLoanTest::user_dtoken(dave.clone()), 100_0000_0000);
+
+        // current_total_loan = 40_0000_0000 ; current_total_saving = 100_0000_0000;
+        // so current Utilization rate = 40_0000_0000 / (40_0000_0000 + 100_0000_0000) = 0.2857142857142857 ;
+        // so current_total_interest_rate: 0.2857142857142857 * 0.1 + 0.05 = 0.07857142857142857
+        assert_eq!(DepositLoanTest::current_loan_interest_rate(), 7857142);
+        assert_eq!(DepositLoanTest::current_saving_interest_rate(), 3142856); // 7857142 * 40_0000_0000 / 100_0000_0000 = 3142856.8
+        assert_eq!(DepositLoanTest::value_of_tokens(), 1_0000_0000);
+
+
+        // after 1500s, interest will be: 1500 * 7857142 / 10^8 / 365 / 86400 * total_loan = 149.48900304414002
+
+
+        // calculate interest generate by time
+        // 1.0 * 10**8 * (1.0 + 14900.0/10000000000) = 100000149
+        // TODO: add time duration in testcase
+
+        // next_n_block(5u32.into());
+        // assert_eq!(DepositLoanTest::value_of_tokens(), 100000149);
+        // assert_eq!(GenericAssetTest::free_balance(&USDT, &DepositLoanTest::collection_account_id()), 6000014948);
     });
 }
